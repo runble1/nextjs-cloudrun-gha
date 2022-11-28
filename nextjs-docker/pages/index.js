0e74de21fb695fcbe58ea33ec90c2a83bef02b19
runble1 <nishiyama-wataru@dmm.com>
2022-11-29 07:50:39 +0900

    Mod deploy.yml

--- nextjs-docker/pages/index.js
+++ nextjs-docker/pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App7</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!8
         </h1>
 
         <p className={styles.description}>
