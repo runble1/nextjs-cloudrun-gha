f3beb9f7220a63ac877c210948ac7e10da8023ea
runble1 <nishiyama-wataru@dmm.com>
2022-11-29 08:08:46 +0900

    Mod next.js

--- nextjs-docker/pages/index.js
+++ nextjs-docker/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!12
+          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!13
         </h1>
 
         <p className={styles.description}>
