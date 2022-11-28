7d3cada66cc225c6f131ba55b0344c2c8ae6091f
runble1 <nishiyama-wataru@dmm.com>
2022-11-28 16:43:17 +0900

    next.js

--- nextjs-docker/pages/index.js
+++ nextjs-docker/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App4</title>
+        <title>Create Next App5</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
